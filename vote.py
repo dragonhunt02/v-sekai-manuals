4f1e5530766613092b95886508f69c85b80f9880
dragonhunt02 <jackhunt43@libero.it>
2025-03-31 11:45:51 +0200

    Update vote.py

--- vote.py
+++ vote.py
@@ -38,5 +38,13 @@ for ballot in ballot_list:
 
 # Step 2: Compute the average for each key and store in a dictionary
 averages = {key: round(sum(values) / len(values)) for key, values in aggregated_values.items()}
+averages_df = pd.DataFrame(averages, columns=["Task", "Priority"])
 
 print("Averages for each key:", averages)
+
+tasks_df = pd.read_csv("./roadmap/tasks.csv")
+tasks_df["Priority"] = 0
+tasks_df.update(averages_df)
+
+print(averages_df)
+print(tasks_df)
