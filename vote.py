0a3aaf893fbd8473bfc016abef15f63f2fa675c6
dragonhunt02 <jackhunt43@libero.it>
2025-03-31 10:29:32 +0200

    Update vote.py

--- vote.py
+++ vote.py
@@ -1,6 +1,11 @@
 from pathlib import Path
 import pandas as pd
 
+import starvote
+from starvote import hashed_ballots_tiebreaker
+import json
+from collections import defaultdict
+
 folder_path = Path('./roadmap/votes')
 csv_files = folder_path.glob('*.csv')
 
@@ -8,8 +13,19 @@ ballot_list = []
 for csv_file in csv_files:
     df = pd.read_csv(csv_file)
     ballot = df.set_index('Task')['Vote'].to_dict()
-    
-    # Use csv_file.stem to extract the file name without its extension and store in the list
     ballot_list.append(ballot)
 
 print(ballot_list)
+
+# Vote
+seats = 3
+
+results = starvote.election(
+    method=starvote.star if seats < 2 else starvote.allocated,
+    ballots=ballot_list,
+    seats=seats,
+    tiebreaker=hashed_ballots_tiebreaker,
+    maximum_score=100,
+)
+
+print(json.dumps(results, indent=4))
