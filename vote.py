6ac66d7970a58a36048762a7b200a0d2700878e5
dragonhunt02 <jackhunt43@libero.it>
2025-03-31 14:37:26 +0200

    Update vote.py

--- vote.py
+++ vote.py
@@ -6,53 +6,62 @@ from starvote import hashed_ballots_tiebreaker
 import json
 from collections import defaultdict
 
-folder_path = Path('./roadmap/votes')
-csv_files = folder_path.glob('*.csv')
+def get_votes(votes_dir):
+    folder_path = Path(votes_dir)
+    #'./roadmap/votes')
+    csv_files = folder_path.glob('*.csv')
 
-ballot_list = []
-for csv_file in csv_files:
-    df = pd.read_csv(csv_file)
-    ballot = df.set_index('Task')['Vote'].to_dict()
-    ballot_list.append(ballot)
+    ballot_list = []
+    for csv_file in csv_files:
+        df = pd.read_csv(csv_file)
+        ballot = df.set_index('Task')['Vote'].to_dict()
+        ballot_list.append(ballot)
+    print(ballot_list)
+    return ballot_list
 
-print(ballot_list)
+def starvote_election(ballot_list, seats=1):
+    results = starvote.election(
+        method=starvote.star if seats < 2 else starvote.allocated,
+        ballots=ballot_list,
+        seats=seats,
+        tiebreaker=hashed_ballots_tiebreaker,
+        maximum_score=100,
+    )
+    print(json.dumps(results, indent=4))
+    return results
 
-# Vote
-seats = 1
+def generate_avg_table(ballot_list):
+    aggregated_values = defaultdict(list)
 
-results = starvote.election(
-    method=starvote.star if seats < 2 else starvote.allocated,
-    ballots=ballot_list,
-    seats=seats,
-    tiebreaker=hashed_ballots_tiebreaker,
-    maximum_score=100,
-)
+    for ballot in ballot_list:
+        for key, value in ballot.items():
+            if not value.is_integer():
+                raise Exception(f"Ballot vote {key} is not an integer")
+            value = max(0, min(value, 100)) #clamp
+            aggregated_values[key].append(value)
 
-print(json.dumps(results, indent=4))
+    # Compute the average for each key and store in a dictionary
+    averages = {key: round(sum(values) / len(values)) for key, values in aggregated_values.items()}
+    averages_df = pd.DataFrame(list(averages.items()), columns=["Task", "Priority"])
+    averages_df.set_index("Task", drop=False, inplace=True)
 
-aggregated_values = defaultdict(list)
+    print(averages_df.to_string(index=False, header=False))
+    print("Averages for each key:", averages)
 
-for ballot in ballot_list:
-    for key, value in ballot.items():
-        if not value.is_integer():
-            raise Exception(f"Ballot vote {key} is not an integer")
-        value = max(0, min(value, 100)) #clamp
-        aggregated_values[key].append(value)
+    tasks_df = pd.read_csv("./roadmap/tasks.csv")
+    tasks_df["Priority"] = 0
+    tasks_df.set_index("Task", drop=False, inplace=True)
 
-# Step 2: Compute the average for each key and store in a dictionary
-averages = {key: round(sum(values) / len(values)) for key, values in aggregated_values.items()}
-averages_df = pd.DataFrame(list(averages.items()), columns=["Task", "Priority"])
-averages_df.set_index("Task", drop=False, inplace=True)
+    tasks_df.update(averages_df)
 
-print(averages_df.to_string(index=False, header=False))
+    print(averages_df)
+    print(tasks_df)
+    return tasks_df
 
-print("Averages for each key:", averages)
+def run_election(votes_dir):
+    ballot_list = get_votes(votes_dir)
+    starvote_winners = starvote_election(ballot_list, 5)
+    avg_table = generate_avg_table(ballot_list)
+    return { "avg_table": avg_table, "winners": starvote_winners }
 
-tasks_df = pd.read_csv("./roadmap/tasks.csv")
-tasks_df["Priority"] = 0
-tasks_df.set_index("Task", drop=False, inplace=True)
-
-tasks_df.update(averages_df)
-
-print(averages_df)
-print(tasks_df)
+run_election('./roadmap/votes')
